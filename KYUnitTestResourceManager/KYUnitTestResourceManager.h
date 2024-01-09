4640f449d0f15ebb1e276a2fa73d5de53d633803
Kjuly <dev@kjuly.com>
2024-01-09 17:41:17 +0800

    Add KYUnitTestResourceManager.xcodeproj

---
+++ KYUnitTestResourceManager/KYUnitTestResourceManager.h
@@ -0,0 +1,19 @@
+//
+//  KYUnitTestResourceManager.h
+//  KYUnitTestResourceManager
+//
+//  Created by Kjuly on 9/1/2024.
+//  Copyright © 2024 Kaijie Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KYUnitTestResourceManager.
+FOUNDATION_EXPORT double KYUnitTestResourceManagerVersionNumber;
+
+//! Project version string for KYUnitTestResourceManager.
+FOUNDATION_EXPORT const unsigned char KYUnitTestResourceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYUnitTestResourceManager/PublicHeader.h>
+
+
